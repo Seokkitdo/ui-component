9ac68a5ea242424797d9e78ad8da33177b9a2fef
Seokkitdo <sjd12329@gmail.com>
2022-04-29 01:00:57 +0900

    Add : Skeleton UI 완료

--- src/components/Skeleton.tsx
+++ src/components/Skeleton.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from "react";
+import React, { ReactNode, useMemo } from "react";
 import styled from "@emotion/styled/macro";
 import { css, keyframes } from '@emotion/react';
 
@@ -21,13 +21,13 @@ const pulseAnimation = css`
 `
 
 const Base = styled.div<Props>`
-    ${({ color }) => color && `background-color: ${color}`}
-    ${({ rounded }) => rounded && 'border-radius: 8px'}
-    ${({ circle }) => circle && 'border-radius: 50%'}
-    ${({ width, height }) => (width || height) && 'display: block'}
-    ${({ animation }) => animation && pulseAnimation}
-    width: ${({ width, unit }) => width && unit && `${width}${unit}`}
-    height: ${({ height, unit }) => height && unit && `${height}${unit}`}
+    ${({ color }) => color && `background-color: ${color}`};
+    ${({ rounded }) => rounded && 'border-radius: 8px'};
+    ${({ circle }) => circle && 'border-radius: 50%'};
+    ${({ width, height }) => (width || height) && 'display: block'};
+    ${({ animation }) => animation && pulseAnimation};
+    width: ${({ width, unit }) => width && unit && `${width}${unit}`};
+    height: ${({ height, unit }) => height && unit && `${height}${unit}`};
 `
 
 const Content = styled.span`
@@ -38,20 +38,32 @@ interface Props {
     width?: number;
     height?: number;
     circle?: boolean;
-    rounded: boolean;
+    rounded?: boolean;
     count?: number;
     unit?: string;
     animation?: boolean;
     color?: string;
     style?: React.CSSProperties;
+    children?: ReactNode;
 }
 
-const Skeleton: React.FC<Props> = ({ width, height, circle, rounded, count, unit, animation, color, style }) => {
+const Skeleton: React.FC<Props> = ({ width, height, circle, rounded, count, unit = 'px', animation = true, color = '#f4f4f4', style, children }) => {
     const content = useMemo(() =>
         [...Array({ length: count })].map(() => '-').join('')
         , [count])
     return (
-        <Content>{content}</Content>
+        <Base
+            style={style}
+            rounded={rounded}
+            circle={circle}
+            width={width}
+            height={height}
+            animation={animation}
+            unit={unit}
+            color={color}
+        >
+            <Content>{children || content}</Content>
+        </Base>
     )
 }
 
